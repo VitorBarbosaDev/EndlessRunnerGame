c3fbad2090e02e3a3ac19dd9baec00e28f6c7514
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-06-08 12:53:50 +0100

    add checkbounds function to stop player from falling through the floor

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,9 @@ const c = canvas.getContext('2d');
 canvas.width  = 1024;
 canvas.height = 576;
 
+// Define the gravity constant for the game
+const gravity = .5;
+
 class Player {
 	constructor()
 	{
@@ -32,7 +35,33 @@ class Player {
 		this.position.y += this.velocity.y;
 		this.position.x += this.velocity.x;
 		
+		this.checkBounds();
+	}
+	
+	checkBounds(){
+		// Define the padding
+		const padding = 5;
 		
+		// If the player is not at the bottom of the canvas, apply gravity
+		if (this.position.y + this.height + this.velocity.y <= canvas.height)
+			{
+				this.velocity.y += gravity;
+			}
+		// If the player is at the bottom of the canvas, set the vertical velocity to 0
+		else
+			{
+				this.velocity.y = 0;
+			}
+		// If the player is not at the left or right edge of the canvas, apply horizontal velocity
+		if (this.position.x + this.width + this.velocity.x <= canvas.width - padding && this.position.x + this.velocity.x >= padding)
+			{
+				this.velocity.x += 0;
+			}
+		// If the player is at the left or right edge of the canvas, set the horizontal velocity to 0
+		else
+			{
+				this.velocity.x = 0;
+			}
 	}
 }
 
